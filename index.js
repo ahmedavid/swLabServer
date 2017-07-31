bbd350d6c13c677622bface7959a5f4cc47db421
David Ahmadov <david.ahmadov@samsa-it.de>
2017-07-31 19:13:19 +0200

    cors

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var images = __dirname + "/public/images/";
 
 var content = JSON.parse(fs.readFileSync('public/content.json', 'utf8'));
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // viewed at http://localhost:8080
 
 app.get('/', function(req, res) {
